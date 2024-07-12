b5fea0616b0cbbb304ba403a885e6b11573767bf
loganrower <lkrower2000@gmail.com>
2024-07-12 12:45:55 -0700

    witty pi additional testing

---
+++ scripts/poc_scripts/RTC/wittypi_shut_start.py
@@ -0,0 +1,7 @@
+"""
+This script will log and test the Witty Pis ability to automatically shutdown and startup
+
+This script will be utilized in order to test WittyPi 4 hardware performance 
+and mitigate in field failures of sensor systems.
+
+"""
