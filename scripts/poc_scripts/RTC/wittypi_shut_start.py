c2ea034a075cb1873ab86be2d11d3d09c8f09433
loganrower <lkrower2000@gmail.com>
2024-07-12 13:13:34 -0700

    wittpi shutdown testing

--- scripts/poc_scripts/RTC/wittypi_shut_start.py
+++ scripts/poc_scripts/RTC/wittypi_shut_start.py
@@ -4,4 +4,15 @@ This script will log and test the Witty Pis ability to automatically shutdown an
 This script will be utilized in order to test WittyPi 4 hardware performance 
 and mitigate in field failures of sensor systems.
 
+This will be implemented to run on boot....
+
 """
+import sys
+import os
+print(os.getcwd())
+sys.path.append('/home/pi/bee_cam_basic')
+from scripts.sensors import WittyPi
+# initialize wittypi shutdown
+with WittyPi() as witty:
+    witty.shutdown_5min()
+    witty.startup_10min()
